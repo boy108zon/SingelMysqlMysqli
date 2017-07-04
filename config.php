29a9a828d6581e13962aff8cd8a36c69bf2e6171
boy108zon <boy108zon@gmail.com>
2017-07-04 17:24:49 +0530

    database configuration file
    it is a configuration file for database and you can put your configuration variable as per your requirement

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$dbconfig['db_server'] = 'localhost';
+$dbconfig['db_port'] = ':3306';
+$dbconfig['db_username'] = 'root';
+$dbconfig['db_password'] = '';
+$dbconfig['db_name'] = 'test';
+
+$resource_type='MySqli';
+
+?>
